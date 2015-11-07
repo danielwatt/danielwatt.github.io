8a21e6e1d424f148ded9f9d9ae0b61d801267d9c
danielwatt <daniel.watt@live.ca>
2015-11-07 09:19:25 -0500

    Code now disables jquery animation on mobile

--- js/rolling.js
+++ js/rolling.js
@@ -1,25 +1,28 @@
 $(function() {
-	 
-	$(window).scroll(function() {
-		var fraction = $(window).scrollTop()/($("#aboutContainer").offset().top-200);
-		var theta =  fraction*2*Math.PI;
-		var opac = fraction*1.0;
-		var length = Math.PI*150;
-		var position = length - (fraction*length);
-		if(fraction<1){
-			$('.profilePicture').css({ 
-				transform: 'rotate(' + theta + 'rad)',
-				opacity: opac,
-				right: position + 'px'
-				});
-	 	}
-	 	else
-	 	{
-	 		$('.profilePicture').css({ 
-	 			transform: 'rotate(' + 2*Math.PI + 'rad)',
-	 			opacity: 1.0,
-	 			right: 0 + 'px'
-	 			});
-	 	}
-	});
+	var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
+	if (!isMobile){
+		$(window).scroll(function() {
+			var fraction = $(window).scrollTop()/($("#aboutContainer").offset().top-200);
+			var theta =  fraction*2*Math.PI;
+			var opac = fraction*1.0;
+			var length = Math.PI*150;
+			var position = length - (fraction*length);
+			if(fraction<1){
+				$('.profilePicture').css({ 
+					transform: 'rotate(' + theta + 'rad)',
+					opacity: opac,
+					right: position + 'px'
+					});
+		 	}
+		 	else
+		 	{
+		 		$('.profilePicture').css({ 
+		 			transform: 'rotate(' + 2*Math.PI + 'rad)',
+		 			opacity: 1.0,
+		 			right: 0 + 'px'
+		 			});
+		 	}
+
+		});
+	}
 });
